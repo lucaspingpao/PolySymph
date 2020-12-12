1fe6ae2b54833eb3b04035020d7b5d576a5b05af
Lucas <lucaspingpao@gmail.com>
2020-12-12 16:40:10 +0100

    first commit

---
+++ Test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 27 11:34:59 2020
+
+@author: lucaspao
+"""
+
+print("hello world")
+
+print("what's up")
+
+import matplotlib.pyplot as plt
+plt.plot(range(10), 'o')
